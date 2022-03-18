9d8ec7134f3aade32dd3dff089795c2f77eb3d5b
hasan <hasanelmi678@gmail.com>
2022-03-18 17:50:15 +0000

    created "/" path

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,22 @@
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes as Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+// Pages
+import FindJobs from './pages/FindJobs/FindJobs';
+
 
 function App() {
   return (
-    <div>
-      App
-    </div>
+    <Router>
+        <Switch>
+          <Route exact path="/" element={<FindJobs />} />
+        </Switch>
+    </Router>
   );
 }
 
--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import bulma from 'bulma';
 
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
