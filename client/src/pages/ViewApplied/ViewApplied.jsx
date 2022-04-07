fef056b3a3abb8cd90ddb41b961e11fec706e897
hasan <hasanelmi678@gmail.com>
2022-04-07 13:52:34 +0100

    created view applied page

--- client/src/pages/ViewApplied/ViewApplied.jsx
+++ client/src/pages/ViewApplied/ViewApplied.jsx
@@ -4,12 +4,19 @@ import "./viewApplied.scss";
 import { useDispatch, useSelector } from "react-redux";
 import { getAllUsersThatApplied } from "../../actions/interviewActions";
 import { useParams, Link } from "react-router-dom";
+import { getOneInterview } from "../../actions/interviewActions";
 
-import {AiFillGithub, AiOutlineTwitter, AiFillLinkedin} from 'react-icons/ai';
+import { AiFillGithub, AiOutlineTwitter, AiFillLinkedin } from "react-icons/ai";
 
 // Components
 import Navbar from "../../components/Navbar/Navbar";
 import SavedHeader from "../../components/SavedHeader/SavedHeader";
+
+const flags = {
+  UK: "🇬🇧",
+  USA: "🇺🇸",
+  EU: "🇪🇺",
+};
 
 const ViewApplied = ({ darkTheme, setDarkTheme }) => {
   const dispatch = useDispatch();
@@ -22,74 +29,97 @@ const ViewApplied = ({ darkTheme, setDarkTheme }) => {
   const getUsersThatApplied = useSelector((state) => state.getUsersThatApplied);
   const { usersThatApplied, loading } = getUsersThatApplied;
 
+  const oneInterview = useSelector((state) => state.oneInterview);
+  const { interview, loading: loadingInterview } = oneInterview;
+
   useEffect(() => {
     dispatch(getAllUsersThatApplied(jobId));
+    dispatch(getOneInterview(jobId));
     console.log("Dispatch");
   }, []);
 
-  if (loading) return "Loading...";
+  function toTitleCase(str) {
+    return str.replace(/\w\S*/g, function (txt) {
+      return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+    });
+  }
+
+  if (loading || loadingInterview) return "Loading...";
   return (
     <div className={darkTheme ? "view-applied dark" : "view-applied"}>
       {console.log(usersThatApplied, "usersThatApplied")}
       <Navbar darkTheme={darkTheme} setDarkTheme={setDarkTheme} />
       <SavedHeader title="View applicants details" darkTheme={darkTheme} />
       <div className="applications-profiles-container">
+        {interview?.map((job) => (
+        <div className="back-url">
+          {console.log(job, "Int")}
+          <Link to="/applications">Applications</Link> / <p>{job?.jobTitle} - {job?.companyName}</p>
+        </div>
+        ))}
         <div className="profiles">
-            {usersThatApplied?.map((user) => (
-                <div className="user-profile">
-                <div className="user-profile-header">
-                  <h3 className="name">{user.name}</h3>
-                  <h4 className="location">
-                    <span className="flag">🇬🇧</span>
-                    <span className="mx-2">{user.location}</span>
-                  </h4>
-                  <h4 className="location">
-                    <span>{user.email}</span>
-                  </h4>
-                </div>
-                <div className="user-profile-content">
-                  <h4 className="description">
-                      {user.userCV?.aboutMe ? (
-                          <>
-                            {user.userCV?.aboutMe?.length > 100 ? (
-                                <>
-                                {user.userCV?.aboutMe.substring(0, 150) + "....."}
-                                </>
-                            ) : (
-                                <>
-                                {user.userCV?.aboutMe + "...."} 
-                                </>
-                            )}
-                          </>
+          {usersThatApplied?.map((user) => (
+            <div className="user-profile">
+              <div className="user-profile-header">
+                <h3 className="name">{toTitleCase(user.name)}</h3>
+                <h4 className="location">
+                  <span className="flag">{flags[user.location]}</span>
+                  <span className="mx-2">{user.location}</span>
+                </h4>
+                <h4 className="location">
+                  <span>{toTitleCase(user.email)}</span>
+                </h4>
+              </div>
+              <div className="user-profile-content">
+                <h4 className="description">
+                  {user.userCV?.aboutMe ? (
+                    <>
+                      {user.userCV?.aboutMe?.length > 100 ? (
+                        <>{user.userCV?.aboutMe.substring(0, 150) + "....."}</>
                       ) : (
-                          "No description added by the user"
+                        <>{user.userCV?.aboutMe + "...."}</>
                       )}
-                       
-                  </h4>
-                </div>
-                <hr />
-                <div className="user-profile-footer">
-                  <p className="tag github-tag my-1">
+                    </>
+                  ) : (
+                    "No description added by the user"
+                  )}
+                </h4>
+              </div>
+              <hr />
+              <div className="user-profile-footer">
+                {user.userCV?.githubUrl && (
+                  <a
+                    className="tag github-tag my-1 mx-2"
+                    href={`http://github.com/${user.userCV?.githubUrl}`}
+                  >
                     <span className="icon">
                       <AiFillGithub />
                     </span>
                     Github
-                  </p>
-                  <p className="tag twitter-tag mx-2 my-1">
-                  <span className="icon">
+                  </a>
+                )}
+                {user.userCV?.twitterUrl && (
+                  <a
+                    className="tag twitter-tag mx-2 my-1"
+                    href={`http://twitter.com/${user.userCV?.twitterUrl}`}
+                  >
+                    <span className="icon">
                       <AiOutlineTwitter />
                     </span>
                     Twitter
-                  </p>
-                  <p className="tag linkedin-tag my-1">
-                  <span className="icon">
+                  </a>
+                )}
+                {user.userCV?.linkedInUrl && (
+                  <a className="tag linkedin-tag my-1 mx-2" hred="linkedin.com">
+                    <span className="icon">
                       <AiFillLinkedin />
                     </span>
                     LinkedIn
-                  </p>
-                </div>
+                  </a>
+                )}
               </div>
-            ))}
+            </div>
+          ))}
         </div>
       </div>
     </div>
