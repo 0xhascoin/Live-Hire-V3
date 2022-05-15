2ba1ac3521eebf26f8c0bb8c3312d02bc85e5e6f
hasan <hasanelmi678@gmail.com>
2022-05-15 15:55:16 +0100

    created basic success page

--- client/src/components/PostJobSuccess/PostJobSuccess.jsx
+++ client/src/components/PostJobSuccess/PostJobSuccess.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-
+import './successPage.scss';
+import {AiOutlineCheckCircle} from 'react-icons/ai';
 import { useSelector } from "react-redux";
 import {Link} from 'react-router-dom';
 
@@ -10,8 +11,11 @@ const PostJobSuccess = () => {
   if(loading) return "Loading...."
 
   return (
-    <div>
-      <Link to={`/job/${interview?._id}`}>View your job</Link>
+    <div className="success-page">
+      <AiOutlineCheckCircle style={{fontSize: '4rem', color: '#57cc99'}}/>
+      <h1 className="success-title">Thank you for posting a job with us</h1>
+      <p className="success-subtitle">You can view your job using the link below</p>
+      <Link className="button is-primary success-button" to={`/job/${interview?._id}`}>View my job</Link>
     </div>
   )
 }
