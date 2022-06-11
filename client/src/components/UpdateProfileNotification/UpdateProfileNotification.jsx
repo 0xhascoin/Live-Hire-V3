b4149cb2b5046eaf4f02faf5866615e8055fad97
hasanelmi678 <70804507+hasanelmi678@users.noreply.github.com>
2022-06-11 12:32:38 +0100

    Update UpdateProfileNotification.jsx

--- client/src/components/UpdateProfileNotification/UpdateProfileNotification.jsx
+++ client/src/components/UpdateProfileNotification/UpdateProfileNotification.jsx
@@ -1,23 +1,21 @@
 import React from "react";
 import "./updateProfileNotification.scss";
-import { motion } from "framer-motion";
+import { Link } from "react-router-dom";
 import { BsFillArrowUpRightSquareFill } from "react-icons/bs";
 
 const UpdateProfileNotification = ({ darkTheme }) => {
   return (
-    <motion.div
-      initial={{ y: 10, opacity: 0 }}
-      animate={{ y: 0, opacity: 1 }}
-      transition={{ duration: 0.8, delay: 0.1 }}
+    <Link
       className={
         darkTheme ? "notification dark-theme" : "notification light-theme"
       }
+      to={'/account'}
     >
       <span>Update your profile</span>
       <span className="icon">
         <BsFillArrowUpRightSquareFill />
       </span>
-    </motion.div>
+    </Link>
   );
 };
 
